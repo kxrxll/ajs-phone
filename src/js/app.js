7183d1440681dc5a2ea96c478c34c4069ae27c2e
kxrxll <kovalyovkirill@gmail.com>
2021-08-11 14:18:32 +0300

    solved

--- src/js/app.js
+++ src/js/app.js
@@ -1,17 +1,13 @@
 // TODO: write your code here
 class Validator {
   static validatePhoneNumber(str) {
-    if (
-      // поиск запрещенных символов
-      str.search(/[^A-Za-z\d\-_]/g) === -1
-      // поиск запрещенных символов в начале строки
-    && str.search(/^[^\d\-_]/) !== -1
-    // поиск запрещенных символов в конце строки
-    && str.search(/[^\d\-_]$/) !== -1
-    // поиск 4 цифр в строке
-    && str.search(/[\d]{4,}/) === -1) {
-      return true;
-    } return false;
+    const no8 = str.replace(/^8/, '+7');
+    const noOtherSymbols = no8.replace(/([\D])/g, (_zero, first) => {
+      if (first !== '+') {
+        return '';
+      } return '+';
+    });
+    return noOtherSymbols;
   }
 }
 
